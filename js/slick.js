0fd898193747ab9e8c6f42e625c0cd9667cdc6e2
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-09-09 13:58:32 +0300

    2

--- js/slick.js
+++ js/slick.js
@@ -1,1 +1,23 @@
+$(function () {
+  const $slider = $(".i-u-reviews-list");
 
+  $slider.on("init reInit afterChange", function (event, slick, currentSlide) {
+    let i = (currentSlide ? currentSlide : 0) + 1;
+    let progress = (i / slick.slideCount) * 100;
+    $(".progress-fill").css("width", progress + "%");
+  });
+
+  $slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    cssEase: "linear",
+    prevArrow: ".i-u-reviews-list-left",
+    nextArrow: ".i-u-reviews-list-right",
+  });
+});
